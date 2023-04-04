95054e90ee3959dd362f27c3520c4829499fb719
Jean <acalto2@gmail.com>
2023-04-04 21:24:46 +0200

    Custom Exception Class

---
+++ lib/core/exception/failure.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+class Failure with EquatableMixin implements Exception {
+  final String message;
+  final int? statusCode;
+  final Exception? exception;
+  final StackTrace stackTrace;
+
+  Failure({
+    required this.message,
+    this.statusCode,
+    this.exception,
+    this.stackTrace = StackTrace.empty,
+  });
+
+  @override
+  List<dynamic> get props => [message, statusCode, exception, stackTrace];
+
+  @override
+  bool get stringify => true;
+}
