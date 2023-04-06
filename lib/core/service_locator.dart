bdda5bf56edc6fd158c487f10e2c1a5b7e427ab1
Jean <acalto2@gmail.com>
2023-04-06 21:12:31 +0200

    Inject Dependency with Service Locator

--- lib/core/service_locator.dart
+++ lib/core/service_locator.dart
@@ -1,8 +1,32 @@
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart';
 import 'package:todo_app/core/route/go_router_provider.dart';
+import 'package:todo_app/features/todo/application/itodo_service.dart';
+import 'package:todo_app/features/todo/application/todo_service.dart';
+import 'package:todo_app/features/todo/data/api/itodo_api_service.dart';
+import 'package:todo_app/features/todo/data/api/todo_api_service.dart';
+import 'package:todo_app/features/todo/data/repository/itodo_repository.dart';
+import 'package:todo_app/features/todo/data/repository/todo_repository.dart';
+import 'package:todo_app/features/todo/presentation/controller/todo_add_controller.dart';
+import 'package:todo_app/features/todo/presentation/controller/todo_controller.dart';
 
 final getIt = GetIt.instance;
 
 void serviceLocatorInit() {
   getIt.registerFactory(() => GoRouterProvider());
+  getIt.registerSingleton<Client>(Client());
+
+  //Feature ToDo
+  getIt.registerLazySingleton<IToDoApiService>(
+      () => TodoApiService(getIt<Client>()));
+  getIt.registerLazySingleton<IToDoRepository>(
+      () => ToDoRepository(getIt<IToDoApiService>()));
+  getIt.registerLazySingleton<IToDoService>(
+      () => ToDoService(getIt<IToDoRepository>()));
+
+  //ToDo Controller
+  getIt.registerFactory<ToDoController>(
+      () => ToDoController(getIt<IToDoService>()));
+  getIt.registerFactory<ToDoAddController>(
+      () => ToDoAddController(getIt<IToDoService>()));
 }
