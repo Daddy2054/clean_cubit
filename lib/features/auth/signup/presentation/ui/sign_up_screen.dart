b1da06488eb56ade6df30defa1a43f8ea425aea1
Jean <acalto2@gmail.com>
2023-04-17 00:26:04 +0200

    14.Listen SignUp State Change.mp4

--- lib/features/auth/signup/presentation/ui/sign_up_screen.dart
+++ lib/features/auth/signup/presentation/ui/sign_up_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:todo_app/common/mixin/dialog_mixin.dart';
 import 'package:todo_app/common/style/dimens.dart';
 import 'package:todo_app/common/widget/button/primary_button.dart';
 import 'package:todo_app/common/widget/check_box_widget.dart';
@@ -17,7 +19,7 @@ class SignUpScreen extends StatefulWidget {
   State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
-class _SignUpScreenState extends State<SignUpScreen> {
+class _SignUpScreenState extends State<SignUpScreen> with DialogMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
@@ -47,117 +49,128 @@ class _SignUpScreenState extends State<SignUpScreen> {
         child: Padding(
           padding: const EdgeInsets.all(kMedium),
           child: Form(
-            key: _formKey,
+              key: _formKey,
               child: BlocConsumer<SignUpController, SignUpState>(
-            listener: (context, state) {},
-            buildWhen: (previous, current) => false,
-            builder: (context, state) {
-              return Column(
-                children: [
-                  const SizedBox(
-                    height: kMedium,
-                  ),
-                  CustomTextFormField(
-                    controller: _nameController,
-                    labelText: 'Name',
-                    hintText: 'Enter your name',
-                    keyboardType: TextInputType.text,
-                    textInputAction: TextInputAction.next,
-                    prefixIcon: const Icon(Icons.account_circle),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'name is empty';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      context.read<SignUpController>().setFormData(
-                            key: 'name',
-                            value: value,
-                          );
-                      return null;
-                    },
-                  ),
-                  const SizedBox(
-                    height: kMedium,
-                  ),
-                  CustomTextFormField(
-                    controller: _emailController,
-                    labelText: 'Email',
-                    hintText: 'Enter your email',
-                    keyboardType: TextInputType.text,
-                    textInputAction: TextInputAction.next,
-                    prefixIcon: const Icon(Icons.email),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'email is empty';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      context.read<SignUpController>().setFormData(
-                            key: 'email',
-                            value: value,
-                          );
-                      return null;
-                    },
-                  ),
-                  const SizedBox(
-                    height: kMedium,
-                  ),
-                  SignUpPasswordWidget(
-                    formKey: 'password',
-                    passwordController: _passwordController,
-                    labelText: 'Password',
-                    hintText: 'Enter your password',
-                    validatorText: 'Password is empty',
-                  ),
-                  const SizedBox(
-                    height: kMedium,
-                  ),
-                  SignUpPasswordWidget(
-                    formKey: 'password_confirmation',
-                    passwordController: _confirmPasswordController,
-                    labelText: 'Confirm Password',
-                    hintText: 'Confirm your password',
-                    validatorText: 'Confirm password is empty',
-                  ),
-                  const SizedBox(
-                    height: kMedium,
-                  ),
-                  CustomTextFormField(
-                    controller: _phoneController,
-                    labelText: 'Phone',
-                    hintText: 'Enter your phone',
-                    keyboardType: TextInputType.phone,
-                    textInputAction: TextInputAction.next,
-                    prefixIcon: const Icon(Icons.phone),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'phone is empty';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      context.read<SignUpController>().setFormData(
-                            key: 'phone ',
-                            value: value,
-                          );
-                      return null;
-                    },
-                  ),
-                  const SizedBox(
-                    height: kMedium,
-                  ),
-                  const SignUpCheckBox(),
-                  const SizedBox(
-                    height: kMedium,
-                  ),
-                  SignUpButton(onPressed: _signUp),
-                ],
-              );
-            },
-          )),
+                listenWhen: (previous, current) {
+                  return current.isSignUp != previous.isSignUp ||
+                      current.errorMsg != previous.errorMsg;
+                },
+                listener: (context, state) {
+                  if (state.isSignUp) {
+                    _showConfirmDialog();
+                  }
+                  if (state.errorMsg != null) {
+                    _showSnackBar(state.errorMsg ?? 'Something went wrong');
+                  }
+                },
+                buildWhen: (previous, current) => false,
+                builder: (context, state) {
+                  return Column(
+                    children: [
+                      const SizedBox(
+                        height: kMedium,
+                      ),
+                      CustomTextFormField(
+                        controller: _nameController,
+                        labelText: 'Name',
+                        hintText: 'Enter your name',
+                        keyboardType: TextInputType.text,
+                        textInputAction: TextInputAction.next,
+                        prefixIcon: const Icon(Icons.account_circle),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'name is empty';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          context.read<SignUpController>().setFormData(
+                                key: 'name',
+                                value: value,
+                              );
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: kMedium,
+                      ),
+                      CustomTextFormField(
+                        controller: _emailController,
+                        labelText: 'Email',
+                        hintText: 'Enter your email',
+                        keyboardType: TextInputType.text,
+                        textInputAction: TextInputAction.next,
+                        prefixIcon: const Icon(Icons.email),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'email is empty';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          context.read<SignUpController>().setFormData(
+                                key: 'email',
+                                value: value,
+                              );
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: kMedium,
+                      ),
+                      SignUpPasswordWidget(
+                        formKey: 'password',
+                        passwordController: _passwordController,
+                        labelText: 'Password',
+                        hintText: 'Enter your password',
+                        validatorText: 'Password is empty',
+                      ),
+                      const SizedBox(
+                        height: kMedium,
+                      ),
+                      SignUpPasswordWidget(
+                        formKey: 'password_confirmation',
+                        passwordController: _confirmPasswordController,
+                        labelText: 'Confirm Password',
+                        hintText: 'Confirm your password',
+                        validatorText: 'Confirm password is empty',
+                      ),
+                      const SizedBox(
+                        height: kMedium,
+                      ),
+                      CustomTextFormField(
+                        controller: _phoneController,
+                        labelText: 'Phone',
+                        hintText: 'Enter your phone',
+                        keyboardType: TextInputType.phone,
+                        textInputAction: TextInputAction.next,
+                        prefixIcon: const Icon(Icons.phone),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'phone is empty';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          context.read<SignUpController>().setFormData(
+                                key: 'phone ',
+                                value: value,
+                              );
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: kMedium,
+                      ),
+                      const SignUpCheckBox(),
+                      const SizedBox(
+                        height: kMedium,
+                      ),
+                      SignUpButton(onPressed: _signUp),
+                    ],
+                  );
+                },
+              )),
         ),
       ),
     );
@@ -169,5 +182,39 @@ class _SignUpScreenState extends State<SignUpScreen> {
     if (isValid != null && isValid) {
       context.read<SignUpController>().signUp();
     }
+  }
+
+  void _showSnackBar(String errorMsg) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(errorMsg),
+      ),
+    );
+  }
+
+  void _showConfirmDialog() {
+    showConfirmDialog(
+      context: context,
+      title: 'Do you want to login?',
+      msg: 'You will be redirected to the login screen.',
+      btnYesText: 'YES',
+      btnNoText: 'NO',
+      onYesTap: () {
+        //dismiss the dialog
+        final navigator = Navigator.of(context, rootNavigator: true);
+        if (navigator.canPop()) {
+          navigator.pop();
+        }
+        // navigate to login screen
+        context.go('/login');
+      },
+      onNoTap: () {
+        //dismiss the dialog
+        final navigator = Navigator.of(context, rootNavigator: true);
+        if (navigator.canPop()) {
+          navigator.pop();
+        }
+      },
+    );
   }
 }
