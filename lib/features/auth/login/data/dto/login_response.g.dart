f445f452d07d2fc369a4cf30a0799c47fe377b88
Jean <acalto2@gmail.com>
2023-04-17 22:09:07 +0200

    2.Login DTO.mp4

---
+++ lib/features/auth/login/data/dto/login_response.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_LoginResponse _$$_LoginResponseFromJson(Map<String, dynamic> json) =>
+    _$_LoginResponse(
+      accessToken: json['access_token'] as String,
+      userId: json['user_id'] as int,
+      name: json['name'] as String,
+      email: json['email'] as String,
+    );
+
+Map<String, dynamic> _$$_LoginResponseToJson(_$_LoginResponse instance) =>
+    <String, dynamic>{
+      'access_token': instance.accessToken,
+      'user_id': instance.userId,
+      'name': instance.name,
+      'email': instance.email,
+    };
