1871c64da584ebeb1650c50ca7b30a1da6dc0c0a
Jean <acalto2@gmail.com>
2023-04-18 20:08:53 +0200

    2.Setup HydratedBloc.mp4

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:todo_app/core/service_locator.dart';
 import 'package:todo_app/main_widget.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // init the Hydratedbloc
+  HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: kIsWeb
+          ? HydratedStorage.webStorageDirectory
+            : await getApplicationDocumentsDirectory());
   serviceLocatorInit();
   runApp(const MainWidget());
 }
