329e3f7d2c4859102d969a1a2db44afce9779a2a
Jean <acalto2@gmail.com>
2023-04-18 20:54:00 +0200

    6.Setting Screen.mp4

--- lib/features/setting/presentation/ui/setting_screen.dart
+++ lib/features/setting/presentation/ui/setting_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/common/enum/app_theme.dart';
+import 'package:todo_app/features/setting/presentation/controller/setting_controller.dart';
+import 'package:todo_app/features/setting/presentation/state/setting_state.dart';
 
 class SettingScreen extends StatelessWidget {
-  const SettingScreen({super.key});
+  const SettingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +14,21 @@ class SettingScreen extends StatelessWidget {
         title: Text('Setting'),
         centerTitle: true,
       ),
-      body: Column(),
+      body: Column(
+        children: [
+          BlocBuilder<SettingController, SettingState>(
+            builder: (context, state) {
+              return SwitchListTile.adaptive(
+                title: Text('Theme : (${state.appTheme.name.toUpperCase()})'),
+                value: state.appTheme == AppTheme.dark ? true : false,
+                onChanged: (value) {
+                  context.read<SettingController>().setAppTheme();
+                },
+              );
+            },
+          )
+        ],
+      ),
     );
   }
 }
